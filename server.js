dd1d7c2824409c9e83c7a17205072d3c15b55bf6
chumpon-asaneerat <chumponsenate@gmail.com>
2019-06-23 21:53:10 +0700

    Add sample html page (index.html).

--- server.js
+++ server.js
@@ -82,8 +82,45 @@ app.use(bodyparser.urlencoded({ extended: true }));
 const iconpath = path.join(__dirname, "public", "favicon.ico");
 app.use(favicon(iconpath));
 
+const distPath = path.join(__dirname, 'public', 'dist');
+//const distMaxAge = { maxage: '1d' };
+const distMaxAge = { maxage: '15s' };
+//const libMaxAge = { maxage: '15s' };
+const dist_libs = [
+    /* jQuery */
+    { "route": "/dist/js", "path": "jquery-3.3.1" },
+    /* jQuery UI */
+    { "route": "/dist", "path": "jquery-ui-1.12.1" },
+    /* bootstrap 4.x */
+    { "route": "/dist/js", "path": "popperjs-1.14.6" },
+    { "route": "/dist/js", "path": "tooltipjs-1.3.1" },
+    { "route": "/dist", "path": "bootstrap-4.2.1" },
+    /* font-awesome 5.x */
+    { "route": "/dist", "path": "font-awesome-5.9.0" },
+    /* emoji-symbols */
+    { "route": "/dist/css", "path": "emoji-symbols-1.0.0" },
+    /* flag-icon-css 3.x */
+    { "route": "/dist", "path": "flag-icon-css-3.1.0" },
+    /* moment */
+    { "route": "/dist/js", "path": "moment-2.22.2" },
+];
+
+function dist_lib(app, exportRoute, localPath) {
+    console.log('publish "' + localPath + '"');
+    app.use(exportRoute, express.static(path.join(distPath, localPath), distMaxAge));
+};
+
+// dist paths.
+dist_libs.forEach(element => {
+    dist_lib(app, element.route, element.path);
+});
+
 app.get("/", (req, res) => {
     res.status(200).send(`It's work!!!`);
+});
+
+app.get("/:file", (req, res) => {
+    res.sendFile(path.join(__dirname, req.params.file));
 });
 
 /**
